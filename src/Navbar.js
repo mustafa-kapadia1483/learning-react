b1403580c49e3bbd50e361d905aeae69196bdbff
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-01-29 16:11:30 +0530

    initial setup

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Mustafa React Site</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/creat">Create</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
