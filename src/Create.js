fd251ab7f1aa7f5972f9b7a97cc7a8f3c2c66347
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-01-30 21:24:43 +0530

    learned about useHistory hook and how it can be used to go back, forward or redirect to a page

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setisPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -19,6 +21,8 @@ const Create = () => {
     }).then(() => {
       console.log("new blog added");
       setisPending(false);
+      // history.go(-1);
+      history.push("/");
     });
   };
 
