58592f4ace645165173c7a67a42091d416e4ac07
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-01-30 21:42:03 +0530

    delete requests done

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,3 +1,4 @@
+import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
@@ -8,6 +9,16 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
   return (
     <div className="blog-details">
       {isPending && <div>Loading....</div>}
@@ -17,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written By {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
